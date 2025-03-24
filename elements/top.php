41182d2ffae7fb8c428c370a42cb83918646b212
yDewolf <andrersng@gmail.com>
2025-03-24 07:57:24 -0300

    Remake Navbar (WIP)

--- elements/top.php
+++ elements/top.php
@@ -4,47 +4,32 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Fossil and Archeology</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    
+    <link rel="stylesheet" href="css/navbar.css">
     <link rel="stylesheet" href="css/components.css">
     <link rel="stylesheet" href="css/index.css">
-</head>
+</head> 
 <body>
-
-    <div class="topo">
-        <nav class="navbar navbar-expand-lg">
-            <div class="container-fluid">
-                <nav class="navbar">
-                    <div class="container">
-                      <a class="logo" href="?page=home">
-                        <img src="imgs/brand.png" width="150" >
-                      </a>
-                    </div>
-                  </nav>
-              
-              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-              </button>
-              <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <div class="barra">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0"> 
-                  <li class="nav-item">
-                    <a class="nav-link" aria-current="page" href="?page=home">Home</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" aria-current="page" href="?page=support">Support</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" aria-current="page" href="?page=credits">Credits</a>
-                  </li>
-                </div>
-                <div class="right-nav">
-                  <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                      <a class="nav-link" aria-current="page" href="?page=login">Login</a>
-                    </li>   
-              </div>
-             </div>
-            </div>
-        </nav>
+  <div class="navbar">
+    <div class="nav">
+      <div class="nav-left">
+        <img class="nav-logo" src="imgs/brand.png" alt="FossilsAndArcheology">
+      </div>
+      <div class="nav-right">
+        <ul class="nav-items">
+          <li class="nav-item">
+            <a class="nav-link" href="?page=home">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="?page=home">Support</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="?page=home">Credits</a>
+          </li>
+          <li class="nav-item align-right">
+            <a class="nav-link" href="?page=home">Login</a>
+          </li>
+        </ul>
+      </div>
     </div>
+    
+  </div>
